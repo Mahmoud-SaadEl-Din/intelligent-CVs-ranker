7d8177036a5b061d16228b3433851a8338695536
Mahmoud Saad-El-Dien <mahmoud.saadeldin101@gmail.com>
2019-02-27 23:02:42 +0200

    Delete data_optimization.py

--- data_optimization.py
+++
@@ -1,7 +0,0 @@
-# Takes Tuples of Tuples of string
-# returns Tuples of Tuples of optimizted string
-
-
-def optimize_this_data(list_arg):
-    #use NLP information extraction & summarization here
-    return list_arg
